4c9cac336a696eee49b1d242b73cfa93b3784e9d
jucsilvaar <92395354+jucsilvaar@users.noreply.github.com>
2021-10-23 20:35:52 -0500

    Revert "Solución Taller 2 by jucsilvaar"

--- Clase2/README.md
+++ Clase2/README.md
@@ -26,13 +26,9 @@
 - Revise y juegue con el código `PlayerBasicMovement`.
 - Responda las siguientes preguntas:
   - Cuál es la diferencia entre `Input.GetAxis` y `Input.GetAxisRaw`?
-Respuesta: usando el comando "Debug.Log", se observo que la principal diferencia entre `Input.GetAxis` y `Input.GetAxisRaw` es que el primero da un valor continuo entre -1 y 1 sin omitir cifras decimales, mientras que `Input.GetAxisRaw` solo muestra -1, 0 o 1.
   - Cuál se deberia usar? (Pregunta capciosa...).
-Respuesta: Depende del uso que se le desee dar, por ejemplo si se usa `Input.GetAxis` se podroa implementar por ejemplo a un joistick, mientras que el segundo, si o si siempre da -1 o 1, por lo tanto siempre es la misma magnitud de movimiento.
   - Qué hace `input.magnitude`? Por qué es util?
-Respuesta: input magnitude nos da la magnitud del vector (horizontal,0,vertical), es decir, si por ejemplo nos movemos de derecha a izquierda o viceversa siempre obtendermos un valor positivo, y si nos movemos de forma diagonal, se obtiene raiz de 2 positivo, es util en el codigo ya que nos permite saber si hay movimiento o no, independientemente de la dirección.
   - Que significa normalizar un Vector (`Normalize`)? Por qué es util cuando se trabaja con movimiento?
-Respuesta: Normalizar un vector significa dividir el vector por si magnitud, lo cual garantiza que la longitud de este sea de uno, esto es util en movimiento debido a que asegura que el personaje siempre se va a mover una unidad si se mueve de manera diagonal, lo cual facilita calculos.
 - **Reto (Opcional):** Implementar una mécanica de dash en base al código dado.
 
 ### Como hacer la entrega:
--- Unity/intro-videojuegos-app-2021-2/Assets/Scr/PlayerBasicMovement.cs
+++ Unity/intro-videojuegos-app-2021-2/Assets/Scr/PlayerBasicMovement.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,57 +7,46 @@ public class PlayerBasicMovement : MonoBehaviour
     [Header("Movement")]
     [SerializeField]
     private float _maxSpeed = 5f;
-    [SerializeField] 
-    private float _dashSpeed = 10f;
-    [SerializeField] 
-    private float _dashTime = 0.1f;
-
+    [SerializeField]
+    private float _acceleration = 15f;
+    [SerializeField]
+    private float _deceleration = 15f;
+    
+    
     private float _currentSpeed = 0.1f;
-    private Vector3 _lastMovementDirection;
-
-    private Rigidbody _rb;
-    private Vector3 _dirMovement;
-    private float _dashingTimer;
-
-    private void Start()
-    {
-        _rb = GetComponent<Rigidbody>();
-    }
-
+    private Vector3 lastMovementDirection;
+    
     void Update()
     {
+        //What's the difference if we use Input.GetAxisRaw instead?
+        //   Use the console to check the difference: Debug.Log or Debug.LogError
+        //Which one should we use? (GetAxis or GetAxisRaw)
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        bool wantDash = Input.GetKeyDown(KeyCode.LeftShift);
-        //Debug.Log ("Se obtiene:" + horizontal);
-        //Debug.Log ("Se obtiene:" + vertical);
-        Vector3 input = new Vector3(horizontal, 0, vertical);
-        _dirMovement = input.normalized;
 
-        if (wantDash && _dashingTimer <= 0)
+        Vector3 input = new Vector3(horizontal, 0, vertical);
+
+        //What does .magnitude? (Search for Unity Vector magnitude). Why it's useful?
+        if (input.magnitude > 0)
         {
-            _dashingTimer = _dashTime;
+            _currentSpeed += _acceleration * Time.deltaTime;
+            
+            //TODO: Normalize the input to get the direction
+            //What means to normalize a Vector? And why it's useful when we apply movement?
+            lastMovementDirection = input;
+        }
+        else
+        {
+            _currentSpeed -= _deceleration * Time.deltaTime;
         }
 
-        _dashingTimer -= Time.deltaTime;
+        //https://docs.unity3d.com/ScriptReference/Mathf.Clamp.html
+        _currentSpeed = Mathf.Clamp(_currentSpeed, 0f, _maxSpeed);
+        
+        Vector3 velocity = lastMovementDirection * _currentSpeed;
+        Vector3 movement = velocity * Time.deltaTime;
+        
+        transform.position += movement;
     }
-
-    private void FixedUpdate()
-    { 
-        Vector3 velocity = _dirMovement * (_dashingTimer > 0 ? _dashSpeed : _maxSpeed);
-        // if (_dashingTimer > 0)
-        // {
-        //     velocity = _dirMovement * _dashTime;
-        // }
-        // else
-        // {
-        //     velocity = _dirMovement * _maxSpeed;
-        // }
-        Vector3 newPos = _rb.position + velocity * Time.deltaTime; //X = V*T
-        //_rb.MovePosition(newPos);
-        velocity.y = _rb.velocity.y; 
-        _rb.velocity = velocity;
-
-        //transform.position = newPos;
-    }
+    
 }
